570b1968feff128464e71c1cfc7e4f7168179fc3
Janeth Eni <enijaneth@gmail.com>
2023-04-20 04:22:33 +0100

    Pycodestyle

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -6,8 +6,6 @@ from base_caching import BaseCaching
 
 class BasicCache(BaseCaching):
     """Child of BaseCaching, which is a caching system without limits"""
-
-
     def put(self, key, item):
         """ Add an item in the cache
         """
@@ -15,7 +13,6 @@ class BasicCache(BaseCaching):
             self.cache_data[key] = item
         else:
             return
-
 
     def get(self, key):
         """ Get an item by key
--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -7,12 +7,10 @@ from base_caching import BaseCaching
 class FIFOCache(BaseCaching):
     """ A caching system that uses the FIFO algorithm """
 
-
     def __init__(self):
         """ Initialize FIFOCache """
         super().__init__()
         self.keys = []
-
 
     def put(self, key, item):
         """ Add an item in the cache """
@@ -27,7 +25,6 @@ class FIFOCache(BaseCaching):
                 self.keys.pop(0)
         self.cache_data[key] = item
         self.keys.append(key)
-
 
     def get(self, key):
         """ Get an item by key """
