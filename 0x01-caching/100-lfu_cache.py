4c2235f5926f13c282265c332d881861ae0788e8
Janeth Eni <enijaneth@gmail.com>
2023-04-21 07:24:29 +0100

    LFU Caching Update

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -3,26 +3,40 @@
 
 from base_caching import BaseCaching
 from collections import deque
+from collections import defaultdict
 
 
 class LFUCache(BaseCaching):
     """ Least Frequently Used caching system """
 
     def __init__(self):
-        """ Initialize MFUCache """
+        """ Initialize LFUCache """
         super().__init__()
         self.load = deque()
+        self.freq = defaultdict(int)
 
     def put(self, key, item):
         """ Add & / del item in the cache """
         if key is None or item is None:
             return
         if key in self.cache_data:
+            self.freq[key] += 1
             self.load.remove(key)
-        elif len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-            lfuu_key = self.load.pop()
-            del self.cache_data[lfu_key]
-            print(f"DISCARD: {lfu_key}")
+        else:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                min_freq = min(self.freq.values())
+                lfu_keys = [k for k, v in self.freq.items() if v == min_freq]
+                for k in self.load:
+                    if k in lfu_keys:
+                        self.load.remove(k)
+                        break
+                for k in lfu_keys:
+                    if k in self.cache_data:
+                        del self.cache_data[k]
+                        del self.freq[k]
+                        print(f"DISCARD: {k}")
+                        break
+            self.freq[key] = 1
         self.load.append(key)
         self.cache_data[key] = item
 
@@ -30,7 +44,7 @@ class LFUCache(BaseCaching):
         """ Get an item by key """
         if key is None or key not in self.cache_data:
             return None
-        else:
-            self.load.remove(key)
-            self.load.append(key)
-            return self.cache_data[key]
+        self.freq[key] += 1
+        self.load.remove(key)
+        self.load.append(key)
+        return self.cache_data[key]
